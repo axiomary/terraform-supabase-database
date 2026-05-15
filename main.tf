457747ba14e7795f2b9ddae598d8781223f47b3b
Scott Hannan <117687334+scott-hannan@users.noreply.github.com>
2026-05-15 12:26:05 -0700

    feat: Add supabase project settings (#3)

--- main.tf
+++ main.tf
@@ -22,3 +22,19 @@ resource "aws_secretsmanager_secret_version" "db_password_val" {
   secret_id     = aws_secretsmanager_secret.db_password.id
   secret_string = random_password.password.result
 }
+
+resource "supabase_apikey" "api_key" {
+  project_ref = supabase_project.project.id
+  name        = "${var.project_name}-key"
+}
+
+resource "supabase_settings" "production" {
+  count = var.settings_config != null ? 1 : 0
+
+  project_ref = supabase_project.project.id
+  database    = var.settings_config.database != null ? jsonencode(var.settings_config.database) : null
+  network     = var.settings_config.network != null ? jsonencode(var.settings_config.network) : null
+  api         = var.settings_config.api != null ? jsonencode(var.settings_config.api) : null
+  auth        = var.settings_config.auth != null ? jsonencode(var.settings_config.auth) : null
+  storage     = var.settings_config.storage != null ? jsonencode(var.settings_config.storage) : null
+}
